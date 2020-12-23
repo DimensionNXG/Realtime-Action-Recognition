2c777b6f29e41553d4d601d0278cec935268426e
Suraj <hrishabhsuraj52@gmail.com>
2020-12-23 10:32:30 +0530

    Update lib_plot.py

--- utils/lib_plot.py
+++ utils/lib_plot.py
@@ -115,8 +115,9 @@ def draw_action_result(img_display, id, skeleton, str_action_label):
         img_display, "P"+str(id % 10)+": "+str_action_label, (TEST_COL, TEST_ROW), font, fontsize, (0, 0, 255), linewidth, cv2.LINE_AA)
 
 
-def add_white_region_to_left_of_image(img_disp):
+def add_white_region_to_left_of_image(img_raw, img_disp):
     r, c, d = img_disp.shape
-    blank = 255 + np.zeros((r, int(c/4), d), np.uint8)
-    img_disp = np.hstack((blank, img_disp))
+    #Stacking the original image on left
+    #blank = 255 + np.zeros((r, int(c/4), d), np.uint8)
+    img_disp = np.hstack((img_raw, img_disp))
     return img_disp
